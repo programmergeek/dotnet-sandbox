5fbfb8bb8423b24b4cf2d2ce3d39d3b932755c06
Thato Will Joseph <programergeek@gmail.com>
2026-03-17 02:20:02 +0200

    fix: flake fix for dotnet env

--- flake.nix
+++ flake.nix
@@ -6,16 +6,26 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }@inputs: 
+  outputs = { self, nixpkgs, flake-utils }: 
     flake-utils.lib.eachDefaultSystem(system: 
 	let
 	    pkgs = nixpkgs.legacyPackages.${system};
 	in {
 	    devShells.default = pkgs.mkShell {
 		    buildInputs = with pkgs; [
-			dotnetCorePackages.sdk_9_0_1xx
-			dotnetCorePackages.sdk_9_0_1xx-bin
+			dotnet-sdk_10
+			roslyn-ls
+			csharp-ls
 		    ];
+	    shellHook = ''
+		export DOTNET_ROOT="${pkgs.dotnet-sdk_10}/share/dotnet"
+		export PATH=$PATH:$HOME/.dotnet/tools
+
+		# Install easy-dotnet CLI tool if not already present
+		if ! command -v dotnet-easydotnet &> /dev/null; then
+		    dotnet tool install --global easy-dotnet
+		fi
+		'';
 		};
 	}
     );
